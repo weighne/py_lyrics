62bd44256b0437f0bdf5d905e5b86b1c7d02a6e2
weighne <cvickery@posteo.net>
2022-03-10 08:31:00 -0500

    Accidentally got bot banned lol, trying proxy stuff

--- lyrics.py
+++ lyrics.py
@@ -2,10 +2,16 @@ import requests
 import json
 import re
 import string
+import random
+from time import sleep
 from bs4 import BeautifulSoup
 
 songlist_url = "https://www.azlyrics.com/{}/{}.html"
 lyrics_url = "https://www.azlyrics.com/lyrics/{}/{}.html"
+agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) \
+        Gecko/20100101 Firefox/24.0'
+headers = {'User-Agent': agent}
+proxy_list = ["http://130.185.119.20:3128","http://99.71.86.199:8118"]
 
 
 def print_list_in_sections(list, length=10):
@@ -44,43 +50,55 @@ def check_band_page(page, band_name_input):  # check that requests actually find
 
 
 def get_lyrics(page):
-    re_pattern = r"<!--([\w\W]*)<br>"
+    re_pattern = r"<!-- U([\w\W]*?)<\/div>"
     re_pattern2 = r"<!--.*?-->"
-    re_pattern3 = r"<br>|</div>"
+    re_pattern3 = r"<br>|</div>|.*?-->"
 
-    # TODO: fix this vvvvv
-    lyrics = re.findall(re_pattern, page.text)  # this is not doing anything???
-    print(lyrics[0])
-    lyrics = re.sub(re_pattern2,'',lyrics[0])
+    lyrics = re.findall(re_pattern, page.text)
+    # lyrics = re.sub(re_pattern2,'',lyrics[0])
     lyrics = re.sub(re_pattern3,'',lyrics[0])
+    lyrics = re.sub("&quot;","\"",lyrics[0])
 
     print(lyrics)
 
 
-
-
 if __name__ == "__main__":
-    band_name_input = input("Enter Band Name: ")
-    band_name = band_name_input.lower().strip().replace(" ","")
-    band_page = requests.get(songlist_url.format(band_name[0],band_name))
+    u_session = requests.session()
+    # proxy_choice = random.randint(1,len(proxy_list)-1)
+    # proxies = {"http":proxy_list[proxy_choice],"https":proxy_list[proxy_choice]}
+    y=0
+    while y <= 10:
+        proxy_choice = random.randint(1,len(proxy_list)-1)
+        proxies = {"http":proxy_list[proxy_choice],"https":proxy_list[proxy_choice]}
+        print(proxies)
+        sleep(2)
+        # band_name_input = input("Enter Band Name: ")
+        band_name_input = "modest mouse"
+        band_name = "modestmouse"
+        # band_name = band_name_input.lower().strip().replace(" ","")
+        band_page = u_session.get(songlist_url.format(band_name[0],band_name), headers=headers,proxies=proxies)
+        print(band_page.text)
 
-    if check_band_page(band_page.text, band_name_input) == True:
-        # print("That's a real page!")
-        re_pattern = '(s:\".*?\")'
-        lyrics_list = re.findall(re_pattern, band_page.text)
-        lyrics_list_formatted = [x.split(":")[1].replace("\"","") for x in lyrics_list]
-        lyrics_list_nonhuman = [re.sub(r'[^\w_]', '', x).lower() for x in lyrics_list_formatted]
+        if check_band_page(band_page.text, band_name_input) == True:
+            # print("That's a real page!")
+            re_pattern = '(s:\".*?\")'
+            lyrics_list = re.findall(re_pattern, band_page.text)
+            lyrics_list_formatted = [x.split(":")[1].replace("\"","") for x in lyrics_list]
+            lyrics_list_nonhuman = [re.sub(r'[^\w_]', '', x).lower() for x in lyrics_list_formatted]
 
-        song_index = print_list_in_sections(lyrics_list_formatted)
+            # song_index = print_list_in_sections(lyrics_list_formatted)
+            song_index = 12 # random.randint(1,100)
 
-        lyrics_page = requests.get(lyrics_url.format(band_name,lyrics_list_nonhuman[int(song_index)]))
-        # print(lyrics_page.text)
-        with open("lyrics_dump.txt","w") as out_file:
-            out_file.write(lyrics_page.text)
-        get_lyrics(lyrics_page)
-        #do some stuff
-    else:
-        print("Goodbye!")
+            lyrics_page = u_session.get(lyrics_url.format(band_name,lyrics_list_nonhuman[int(song_index)]), headers=headers,proxies=proxies)
+            u_session.close()
+            # print(lyrics_page.text)
+            with open("lyrics_dump.txt","w") as out_file:
+                out_file.write(lyrics_page.text)
+            get_lyrics(lyrics_page)
+            #do some stuff
+        else:
+            print("Goodbye!")
+        y+=1
 
 
 # print(band_page)
